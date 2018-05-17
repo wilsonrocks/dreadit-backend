16e11ac6ade97341728b9b916b37ef05b8170806
James Wilson <jwilsoncalderdale@gmail.com>
2018-05-17 09:56:43 +0100

    make seed.js take command line argument to seed different databases and handle errors if the seed JSONs aren't there

--- seed/seed.js
+++ seed/seed.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const models = require('../models');
 
+const tag = process.argv[2];
+
 function getID (collection, key, value) {
     // returns the objectID from the given collection when the value given by key matches value.
     // returns null if not there.
@@ -9,32 +11,44 @@ function getID (collection, key, value) {
 }
 
 async function seedDatabase (tag) {
-    await mongoose.connect(`mongodb://localhost/northcoders_news_${tag}`);
-    await mongoose.connection.dropDatabase();
+    try {
+        await mongoose.connect(`mongodb://localhost/northcoders_news_${tag}`);
+        await mongoose.connection.dropDatabase();
 
-    const users = await models.User.insertMany(require(`./${tag}Data/users.json`));
-    const topics = await models.Topic.insertMany(require(`./${tag}Data/topics.json`));
+        const users = await models.User.insertMany(require(`./${tag}Data/users.json`));
+        const topics = await models.Topic.insertMany(require(`./${tag}Data/topics.json`));
 
-    const rawArticles = require(`./${tag}Data/articles.json`);
-    let articles = rawArticles.map(function(obj) {
-        return {...obj,
-            created_by: getID(users, 'username', obj.created_by),
-            belongs_to: getID(topics, 'slug', obj.topic),
-        };
-    });
-    articles = await models.Article.insertMany(articles);
+        const rawArticles = require(`./${tag}Data/articles.json`);
+        let articles = rawArticles.map(function(obj) {
+            return {...obj,
+                created_by: getID(users, 'username', obj.created_by),
+                belongs_to: getID(topics, 'slug', obj.topic),
+            };
+        });
+        articles = await models.Article.insertMany(articles);
 
-    const rawComments = require(`./${tag}Data/comments.json`);
-    let comments = rawComments.map(function (obj) {
-        return {...obj,
-            belongs_to: getID(articles, 'title', obj.belongs_to),
-            created_by: getID(users, 'username', obj.created_by)
-        };
-    });
-    comments = await models.Comment.insertMany(comments);
+        const rawComments = require(`./${tag}Data/comments.json`);
+        let comments = rawComments.map(function (obj) {
+            return {...obj,
+                belongs_to: getID(articles, 'title', obj.belongs_to),
+                created_by: getID(users, 'username', obj.created_by)
+            };
+        });
+        comments = await models.Comment.insertMany(comments);
+    }
 
-    mongoose.disconnect();
+    catch (err) {
+        console.error(err);
+        console.error('Disconnecting from Mongo');
+    }
+
+    finally {
+        mongoose.disconnect();
+    }
 }
 
-seedDatabase('dev')
-.catch(err => console.log(err));
+seedDatabase(tag)
+.catch(function(err) {
+
+    mongo.disconnect();
+});
