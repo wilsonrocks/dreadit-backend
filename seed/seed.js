47c2bcd8d8d2b5d365bb761906890ed8cbb92670
James Wilson <jwilsoncalderdale@gmail.com>
2018-05-17 09:36:23 +0100

    seeding now adds comments with appropriate references

--- seed/seed.js
+++ seed/seed.js
@@ -8,24 +8,30 @@ function getID (collection, key, value) {
     return answer ? answer._id : null;
 }
 
-
-
 async function init () {
     await mongoose.connect('mongodb://localhost/northcodersNews');
     await mongoose.connection.dropDatabase();
+
     const users = await models.User.insertMany(require('./devData/users.json'));
     const topics = await models.Topic.insertMany(require('./devData/topics.json'));
-    const rawArticles = require('./devData/articles.json');
-    const rawComments = require('./devData/comments.json');
 
+    const rawArticles = require('./devData/articles.json');
     let articles = rawArticles.map(function(obj) {
         return {...obj,
             created_by: getID(users, 'username', obj.created_by),
             belongs_to: getID(topics, 'slug', obj.topic),
         };
     });
-
     articles = await models.Article.insertMany(articles);
+
+    const rawComments = require('./devData/comments.json');
+    let comments = rawComments.map(function (obj) {
+        return {...obj,
+            belongs_to: getID(articles, 'title', obj.belongs_to),
+            created_by: getID(users, 'username', obj.created_by)
+        };
+    });
+    comments = await models.Comment.insertMany(comments);
 
     mongoose.disconnect();
 }
