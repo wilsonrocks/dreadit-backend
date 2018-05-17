9fc817718db3d910ccd9c92fccac3973851151a1
James Wilson <jwilsoncalderdale@gmail.com>
2018-05-17 21:56:19 +0100

    refactor routes into separate files

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 router.get('', function (req, res, next) {
     return res.sendFile('./doc.html', {root:__dirname});
-})
+});
+
+router.use('/users', require('./users.routes'));
+router.use('/topics', require('./topics.routes'));
+router.use('/comments', require('./comments.routes'));
+router.use('/articles', require('./articles.routes'));
 
 module.exports = router;
---
+++ routes/articles.routes.js
@@ -0,0 +1,6 @@
+const app = require('express').Router();
+
+
+
+
+module.exports = app;
