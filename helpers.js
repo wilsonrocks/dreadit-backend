29f586e53bedcd4534aabcaeb730fc061b1f7047
James Wilson <jwilsoncalderdale@gmail.com>
2018-06-20 23:45:33 +0100

    remove unneeded helper functions

--- helpers.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash');
-
-const topicFilter = topic => _.pick(topic, ['_id', 'title', 'slug']);
-const userFilter = user => _.pick(user, ['_id', 'name', 'username', 'avatar_url']);
-
-const articleFilter = article => _.pick(article, ['_id', 'title', 'body', 'belongs_to', 'votes', 'created_by']);
-const commentFilter = comment => _.pick(comment, ['created_at', '_id', 'body', 'belongs_to', 'created_by', 'votes']);
-
-module.exports = {topicFilter, articleFilter, commentFilter, userFilter};
